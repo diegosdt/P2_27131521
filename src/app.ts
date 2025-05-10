c4a3d7925c0280bc3321a4746f9335460d7f1ad4
diegosdt <diegotua066@gmail.com>
2025-05-10 13:38:36 -0400

    subiendo task-2

--- src/app.ts
+++ src/app.ts
@@ -8,29 +8,29 @@ import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
-const port = 3000; // Puerto definido aquí
+const port = 3000; 
 
-// Configuración del motor de vistas
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middlewares
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Manejo de errores 404
+
 app.use((req: Request, res: Response, next: NextFunction) => {
     next(createError(404));
 });
 
-// Manejador de errores global
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,7 +38,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.render('error');
 });
 
-// Iniciar el servidor
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
